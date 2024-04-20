5528ed8439dd3b50321a234366edc19c71fa5632
Laziz <hamroyevlaziz007@gmail.com>
2024-04-20 17:44:11 +0500

    create homebase

---
+++ Home.UI/Domain/Entitys/Home/HomeBase.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entitys.Regions.District;
+using Domain.Entitys.Regions.Province;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entitys.Home
+{
+    public class HomeBase
+    {
+        public int Id { get; set; }
+        public int Arena { get; set; }
+        public long Price { get; set; }
+        public string PhotoPath { get; set; }
+        public string AddressDistrict { get; set; }
+        public string InformationUniversal { get; set; }
+        public string PhoneNumber { get; set; }   /// 998900220407
+    }
+}
---
+++ Home.UI/Domain/Entitys/Home/HotelRent.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entitys.Home
+{
+    public class HotelRent : HomeBase
+    {
+        public int YearBuilt { get; set; }
+        public int NumberRoom { get; set; }
+        public int ResidenceFloors { get; set; }
+
+        public int ResidenceFloorRent { get; set; }
+
+    }
+}
