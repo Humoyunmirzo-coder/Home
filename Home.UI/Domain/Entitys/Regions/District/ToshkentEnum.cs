387dca8c7bdc65547f82dbe5bfe75aeeadfca3a3
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-04-20 14:37:11 +0500

    Cerate structure Cleancode

---
+++ Home.UI/Domain/Entitys/Regions/District/ToshkentEnum.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entitys.Regions.District
+{
+    public enum ToshkentEnum
+    {
+        Bektemir,
+        Chilonzor,
+        Hamza,
+        Mirobod,
+        MirzoUlugbek,
+        Sergeli,
+        SobirRahimov,
+        Uchtepa,
+        Shayxontohur,
+        Yashnobod,
+    }
+}
