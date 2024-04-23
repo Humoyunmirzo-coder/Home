ee396e1e463c94ab30a41cfdc7dfa79fd22941f7
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-04-23 16:54:18 +0500

    Create Interface

--- Home.UI/Aplication/Service/IHomeService.cs
+++ Home.UI/Aplication/Service/IHomeService.cs
@@ -1,20 +1,22 @@
 ﻿using Domain.Entitys.Home;
 using Domain.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aplication.Service
 {
-    public  interface IHomeService 
+    public interface IHomeService
     {
-        Task<Response < Home>> CreateHomeAynce (Home home);
-        Task<Response < Home>> DeleteHomeAynce (int  Id);
-        Task<Response < Home>> UpdateHomeAynce ( Home home );
-        Task<Response < Home>> GetByIdHomeAynce (int  Id);
-        Task<Response <List< Home>>> GetAllHomeAynce ();
+        Task<Response<Home>> CreateHomeAynce(Home home);
+        Task<Response<Home>> DeleteHomeAynce(int Id);
+        Task<Response<Home>> UpdateHomeAynce(Home home);
+        Task<Response<Home>> GetByIdHomeAynce(int Id);
+        Task<Response<List<Home>>> GetAllHomeAynce();
+
+
+        Task<Response<HomeRent>> CreateHomeRentAynce(Home home);
+        Task<Response<HomeRent>> DeleteHomeRentAynce(int Id);
+        Task<Response<HomeRent>> UpdateHomeRentAynce(Home home);
+        Task<Response<HomeRent>> GetByIdHomeRentAynce(int Id);
+        Task<Response<List<HomeRent>>> CreateHomeRentAynce();
 
 
 
