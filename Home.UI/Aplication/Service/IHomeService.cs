190218f15f48bb281ed68741510dcb1f966d450c
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-04-23 17:08:41 +0500

    create service

--- Home.UI/Aplication/Service/IHomeService.cs
+++ Home.UI/Aplication/Service/IHomeService.cs
@@ -1,4 +1,6 @@
 ﻿using Domain.Entitys.Home;
+using Domain.Entitys.Home.Services;
+using Domain.Entitys.Home.SpareParts;
 using Domain.Model;
 
 namespace Aplication.Service
@@ -18,6 +20,37 @@ namespace Aplication.Service
         Task<Response<HomeRent>> GetByIdHomeRentAynce(int Id);
         Task<Response<List<HomeRent>>> CreateHomeRentAynce();
 
+
+         Task<Response<Hotel>> CreateHotelAynce(Home home);
+        Task<Response<Hotel>> DeleteHotelAynce(int Id);
+        Task<Response<Hotel>> UpdateHotelAynce(Home home);
+        Task<Response<Hotel>> GetByIdHotelAynce(int Id);
+        Task<Response<List<Hotel>>> CreateHotelAynce();
+        
+        
+        
+        
+        Task<Response<HomeBuild>> CreateHomeBuildAynce(Home home);
+        Task<Response<HomeBuild>> DeleteHomeBuildAynce(int Id);
+        Task<Response<HomeBuild>> UpdateHomeBuildAynce(Home home);
+        Task<Response<HomeBuild>> GetByIdHomeBuildAynce(int Id);
+        Task<Response<List<HomeBuild>>> CreateHomeBuildAynce();
+               
+
+        Task<Response<HomeRepair>> CreateHomeRepairAynce(Home home);
+        Task<Response<HomeRepair>> DeleteHomeRepairAynce(int Id);
+        Task<Response<HomeRepair>> UpdateHomeRepairAynce(Home home);
+        Task<Response<HomeRepair>> GetByIdHomeRepairAynce(int Id);
+        Task<Response<List<HomeRepair>>> CreateHomeRepairAynce();
+        
+
+        Task<Response<TechnicalServices>> CreateTechnicalServicesAynce(Home home);
+        Task<Response<TechnicalServices>> DeleteTechnicalServicesAynce(int Id);
+        Task<Response<TechnicalServices>> UpdateTechnicalServicesAynce(Home home);
+        Task<Response<TechnicalServices>> GetByIdTechnicalServicesAynce(int Id);
+        Task<Response<List<TechnicalServices>>> CreateTechnicalServicesAynce();
+
+  
 
 
 
