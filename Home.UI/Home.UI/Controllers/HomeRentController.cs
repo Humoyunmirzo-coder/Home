7d2fcde034256b75af5f3e59411e5ea744ea66ff
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-04-27 15:28:13 +0500

    create homerent controller

--- Home.UI/Home.UI/Controllers/HomeRentController.cs
+++ Home.UI/Home.UI/Controllers/HomeRentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Aplication.Service;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Home.UI.Controllers
@@ -7,6 +8,23 @@ namespace Home.UI.Controllers
     [ApiController]
     public class HomeRentController : ControllerBase
     {
+        private readonly IHomeService _homeService;
+
+        public HomeRentController(IHomeService homeService)
+        {
+            _homeService = homeService;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdHome(int id)
+        {
+            var response = await _homeService.GetByIdHomeAynce(id);
+            if (response.StatusCode == 200)
+                return Ok(response);
+            return NotFound(response);
+        }
+
 
     }
 }
