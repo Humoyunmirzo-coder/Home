c2d3b46abd692817186f66683a530a9d574135a5
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-04-29 10:32:21 +0500

    add hotel controoller

---
+++ Home.UI/Home.UI/Controllers/HotelController.cs
@@ -0,0 +1,29 @@
+﻿using Aplication.Service;
+using Infrastructure.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Home.UI.Controllers
+{
+    [Route("/[controller]/[action]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        private readonly  IHomeService _homeService;
+
+        public HotelController(IHomeService homeService)
+        {
+            _homeService = homeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllHotelRent()
+        {
+            var response = await _homeService.GetAllHotelAynce();
+            if (response.StatusCode == 200)
+                return Ok(response);
+            return NotFound(response);
+        }
+
+    }
+}
