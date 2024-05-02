2c1300954b16d81cf0d5a46583f88791b1c0d8f6
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-05-02 11:23:08 +0500

    Create Controller homerepair

---
+++ Home.UI/Home.UI/Controllers/HomeRepairController.cs
@@ -0,0 +1,20 @@
+﻿using Aplication.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Home.UI.Controllers
+{
+    [Route("/[controller]/[action]")]
+    [ApiController]
+    public class HomeRepairController : ControllerBase
+    {
+        private readonly IHomeService   _homeService;
+
+        public HomeRepairController(IHomeService homeService)
+        {
+            _homeService = homeService;
+        }
+
+
+    }
+}
