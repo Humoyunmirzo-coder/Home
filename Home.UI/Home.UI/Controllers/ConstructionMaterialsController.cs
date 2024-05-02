a79c0af7e3a8dcf53adee7c8b7ff56b68ae2418f
BakhodirovDev <bbahodirov.dev@gmail.com>
2024-05-02 12:21:20 +0500

    HomeApplianceController and ConstructionMaterialsController Add

--- Home.UI/Home.UI/Controllers/ConstructionMaterialsController.cs
+++ Home.UI/Home.UI/Controllers/ConstructionMaterialsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Aplication.Service;
+using Domain.Entitys.Home.SpareParts;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Home.UI.Controllers
@@ -7,5 +9,59 @@ namespace Home.UI.Controllers
     [ApiController]
     public class ConstructionMaterialsController : ControllerBase
     {
+        private readonly IHomeService _homeService;
+
+        public ConstructionMaterialsController(IHomeService homeService)
+        {
+            _homeService = homeService;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllConstructionMaterials()
+        {
+            var response = await _homeService.GetAllConstructionMaterialsAynce();
+            if (response.StatusCode == 200)
+                return Ok(response);
+            return NotFound(response);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdConstructionMaterials(int id)
+        {
+            var response = await _homeService.GetByIdConstructionMaterialsAynce(id);
+            if (response.StatusCode == 200)
+                return Ok(response);
+            return NotFound(response);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateHome([FromBody] ConstructionMaterials ConstructionMaterials)
+        {
+            var response = await _homeService.UpdateConstructionMaterialsAynce(ConstructionMaterials);
+            if (response.StatusCode == 200)
+                return Ok(response);
+            return BadRequest(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateHome([FromBody] ConstructionMaterials ConstructionMaterials)
+        {
+            var response = await _homeService.CreateConstructionMaterialsAynce(ConstructionMaterials);
+
+            if (response.StatusCode == 200)
+                return Ok(response);
+            return BadRequest(response);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHome(int id)
+        {
+            var response = await _homeService.DeleteConstructionMaterialsAynce(id);
+            if (response.StatusCode == 200)
+                return Ok(response);
+            return BadRequest(response);
+        }
     }
 }
