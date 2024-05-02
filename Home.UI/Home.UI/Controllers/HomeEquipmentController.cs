2b5041e072774be5db0784844a41398fde5f2e15
BakhodirovDev <bbahodirov.dev@gmail.com>
2024-05-02 11:59:41 +0500

    .

---
+++ Home.UI/Home.UI/Controllers/HomeEquipmentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Home.UI.Controllers
+{
+    [Route("/[controller]/[action]")]
+    [ApiController]
+    public class HomeEquipmentController : ControllerBase
+    {
+    }
+}
