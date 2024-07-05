6f7a42488d961ea6f43af782adcc24f4750846d1
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-07-05 14:01:11 +0500

    add home build

---
+++ Infrastructure/Service/HomeBuildService.cs
@@ -0,0 +1,39 @@
+﻿using Aplication.Service;
+using Domain.Entitys.Home.Services;
+using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Service
+{
+    public class HomeBuildService : IHomeBuild
+    {
+        public Task<Response<HomeBuild>> CreateHomeBuildAynce(HomeBuild homeBuild)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<bool>> DeleteHomeBuildAynce(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<List<HomeBuild>>> GetAllHomeBuildAynce()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<HomeBuild>> GetByIdHomeBuildAynce(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<HomeBuild>> UpdateHomeBuildAynce(HomeBuild homeBuild)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
