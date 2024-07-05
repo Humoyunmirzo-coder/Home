d0a485620f852b069e53a51f83f77dae1c319ed4
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-07-05 13:59:14 +0500

    add repair servise

---
+++ Infrastructure/Service/HomeRepairServices.cs
@@ -0,0 +1,39 @@
+﻿using Aplication.Service;
+using Domain.Entitys.Home.Services;
+using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Service
+{
+    public class HomeRepairServices : IHomeRepair
+    {
+        public Task<Response<HomeRepair>> CreateHomeRepairAynce(HomeRepair homeRepair)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<HomeRepair>> DeleteHomeRepairAynce(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<List<HomeRepair>>> GetAllHomeRepairAynce()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<HomeRepair>> GetByIdHomeRepairAynce(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<HomeRepair>> UpdateHomeRepairAynce(HomeRepair homeRepair)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
