de76afee3f83ac3a66e7db5223b5e17ec95adfc6
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-07-08 10:15:42 +0500

    add home appliance service

--- Infrastructure/Service/HomeApplianceService.cs
+++ Infrastructure/Service/HomeApplianceService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Aplication.Service;
+using Domain.Entitys.Home.SpareParts;
+using Domain.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,31 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Service
 {
-    public  class HomeApplianceService
+    public class HomeApplianceService : IHomeAppliance
     {
+        public Task<Response<HomeAppliance>> CreateHomeApplianceAynce(HomeAppliance homeAppliance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<HomeAppliance>> DeleteHomeApplianceAynce(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<List<HomeAppliance>>> GetAllHomeApplianceAynce()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<HomeAppliance>> GetByIdHomeApplianceAynce(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<HomeAppliance>> UpdateHomeApplianceAynce(HomeAppliance homeAppliance)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
