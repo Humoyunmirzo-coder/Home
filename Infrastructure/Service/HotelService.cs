41a7bad340bc7d04eaa17bb2bcae83188eabcd37
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-07-18 20:52:38 +0500

    add hotel service

---
+++ Infrastructure/Service/HotelService.cs
@@ -0,0 +1,39 @@
+﻿using Aplication.Service;
+using Domain.Entitys.Home;
+using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Service
+{
+    public class HotelService : IHotelService
+    {
+        public Task<Response<Hotel>> CreateHotelAynce(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Hotel>> DeleteHotelAynce(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<List<Hotel>>> GetAllHotelAynce()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Hotel>> GetByIdHotelAynce(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Hotel>> UpdateHotelAynce(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
