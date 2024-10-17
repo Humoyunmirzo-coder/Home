df494918338c585099c7e0d58087b6f23ece79d7
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-10-17 21:33:12 +0500

    change code

--- Domain/Entitys/Home.Services/HomeRepair.cs
+++ Domain/Entitys/Home.Services/HomeRepair.cs
@@ -10,6 +10,7 @@ namespace Domain.Entitys.Home.Services
     public  class HomeRepair  :ServiceBase
     {
         [Key]
+
         public int Id { get; set; }
        public string SizeType { get; set; }
     }
