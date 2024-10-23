5304fa1be43017ad0ed3d4daeaef7f4161ecd44c
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-10-23 22:23:23 +0500

    add coment

--- Domain/Entitys/Home.Services/ServiceBase/ServiceBase.cs
+++ Domain/Entitys/Home.Services/ServiceBase/ServiceBase.cs
@@ -2,6 +2,8 @@
 using Domain.Entitys.Regions.Province;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +12,24 @@ namespace Domain.Entitys.Home.Services
 {
     public  class ServiceBase
     {
+        [Required]
+        [StringLength(30)]
         public decimal Price { get; set; }
+        [Column("moneyType")]
         public string MoneyType { get; set; }
-
+        [Column("photoPath")]
         public string PhotoPath { get; set; }
+        [Column("theemployerandemployee")]
         public string TheEmployerAndEmployee { get; set; }
+        [Column("servicetype")]
         public string ServiceType { get; set; }
+        [Column("addressdistrict")]
         public ToshkentEnum AddressDistrict { get; set; }
+        [Column("addressprovince")]
         public ProvinceEnum AddressProvince { get; set; }
+        [Column("informationuniversal")]
         public string InformationUniversal { get; set; }
+        [Column("phonenumbers")]
         public string PhoneNumbers { get; set; }   /// 998900220407   
     }
 }
